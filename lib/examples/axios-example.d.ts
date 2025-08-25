cd8045d40cf85e7f9672b3bbd5e98d42485bad10
Samir Ibrahim <tech@samirify.com>
2025-08-26 00:20:47 +0100

    chore: publish @samirify/searchbox@1.0.0-alpha.7

---
+++ lib/examples/axios-example.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+declare const AxiosExample: React.FC;
+declare const CustomAxiosExample: React.FC;
+declare const RequestConfigExample: React.FC;
+declare const TransformersExample: React.FC;
+export { AxiosExample, CustomAxiosExample, RequestConfigExample, TransformersExample };
+//# sourceMappingURL=axios-example.d.ts.map
