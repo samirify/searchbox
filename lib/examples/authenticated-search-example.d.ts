9e631e06b1e15d4d9154c9d16a744ee84d35e6c5
Samir Ibrahim <tech@samirify.com>
2025-08-25 23:39:41 +0100

    chore: publish @samirify/searchbox@1.0.0-alpha.4

---
+++ lib/examples/authenticated-search-example.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+declare const AuthenticatedSearchBox: React.FC;
+declare const RoleBasedSearchBox: React.FC;
+export { AuthenticatedSearchBox, RoleBasedSearchBox };
+//# sourceMappingURL=authenticated-search-example.d.ts.map
